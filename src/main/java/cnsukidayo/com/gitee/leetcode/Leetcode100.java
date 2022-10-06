58155c7f9e97b992719e27bd79997c4a9228b19b
sukidayo <2985384723@qq.com>
2022-10-06 16:42:41 +0800

    完成leetcode100相同的数的代码和题解编写

---
+++ src/main/java/cnsukidayo/com/gitee/leetcode/Leetcode100.java
@@ -0,0 +1,51 @@
+package cnsukidayo.com.gitee;
+
+import javax.swing.tree.TreeNode;
+
+/**
+ * @author: sukidayo
+ * @date: 2022/10/6 15:01
+ */
+public class Leetcode100 {
+boolean result = true;
+
+public boolean isSameTree(TreeNode p, TreeNode q) {
+    if (p == null && q == null) {
+        return true;
+    } else if (p == null || q == null) {
+        return false;
+    }
+    if (p.val != q.val) {
+        result = false;
+    }
+    if (p.left != null && q.left != null) {
+        isSameTree(p.left, q.left);
+    } else if (p.left != null || q.left != null) {
+        result = false;
+    }
+    if (p.right != null && q.right != null) {
+        isSameTree(p.right, q.right);
+    } else if (p.right != null || q.right != null) {
+        result = false;
+    }
+    return result;
+}
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
---
+++ src/main/java/cnsukidayo/com/gitee/leetcode/Leetcode111.java
@@ -0,0 +1,8 @@
+package cnsukidayo.com.gitee.leetcode;
+
+/**
+ * @author: sukidayo
+ * @date: 2022/10/6 16:00
+ */
+public class Leetcode111 {
+}
