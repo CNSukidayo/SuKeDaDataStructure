ce759d538eeec6f9692ce10fed0c9ac83bd4caf8
sukidayo <2985384723@qq.com>
2022-10-08 09:17:47 +0800

    完成leetcode263. 丑数代码和题解

--- src/main/java/cnsukidayo/com/gitee/leetcode/Leetcode263.java
+++ src/main/java/cnsukidayo/com/gitee/leetcode/Leetcode263.java
@@ -4,5 +4,22 @@ package cnsukidayo.com.gitee.leetcode;
  * @author: sukidayo
  * @date: 2022/10/7 21:53
  */
-public class Leetcode2 {
+public class Leetcode263 {
+public boolean isUgly(int n) {
+    if (n < 1) {
+        return false;
+    }
+    while (true) {
+        if (n % 5 == 0) {
+            n /= 5;
+        } else if (n % 3 == 0) {
+            n /= 3;
+        } else if (n % 2 == 0) {
+            n /= 2;
+        } else {
+            break;
+        }
+    }
+    return n == 1;
+}
 }
