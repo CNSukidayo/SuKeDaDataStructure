00a6807fa94b6e1873c231a2b463e5269b53e38d
sukidayo <2985384723@qq.com>
2022-10-29 21:19:22 +0800

    完成leetcode973. 最接近原点的 K 个点代码和题解

--- src/main/java/cnsukidayo/com/gitee/leetcode/Leetcode654.java
+++ src/main/java/cnsukidayo/com/gitee/leetcode/Leetcode654.java
@@ -7,29 +7,29 @@ import cnsukidayo.com.gitee.script.TreeNode;
  * @date: 2022/10/22 21:23
  */
 public class Leetcode654 {
-    public TreeNode constructMaximumBinaryTree(int[] nums) {
-        return buildTree(nums, 0, nums.length - 1);
+public TreeNode constructMaximumBinaryTree(int[] nums) {
+    return buildTree(nums, 0, nums.length - 1);
+}
+
+private TreeNode buildTree(int[] nums, int left, int right) {
+    if (left > right) {
+        return null;
+    }
+    if (left == right) {
+        return new TreeNode(nums[left]);
     }
 
-    private TreeNode buildTree(int[] nums, int left, int right) {
-        if (left > right) {
-            return null;
-        }
-        if (left == right) {
-            return new TreeNode(nums[left]);
-        }
+    // 找到最大的节点
+    int maxIndex = left;
+    for (int i = left + 1; i <= right; i++) if (nums[i] > nums[maxIndex]) maxIndex = i;
 
-        // 找到最大的节点
-        int maxIndex = left;
-        for (int i = left + 1; i <= right; i++) if (nums[i] > nums[maxIndex]) maxIndex = i;
+    TreeNode root = new TreeNode(nums[maxIndex]);
 
-        TreeNode root = new TreeNode(nums[maxIndex]);
-
-        // 构造左子树
-        root.left = buildTree(nums, left, maxIndex - 1);
-        // 构造右子树
-        root.right = buildTree(nums, maxIndex + 1, right);
-        return root;
-    }
+    // 构造左子树
+    root.left = buildTree(nums, left, maxIndex - 1);
+    // 构造右子树
+    root.right = buildTree(nums, maxIndex + 1, right);
+    return root;
+}
 
 }
